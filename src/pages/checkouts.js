27df39c133e36b3bcf30e811ccb5c9d18bd7f87c
Pham Hong Thai <hongthaipro@gmail.com>
2021-10-28 21:50:45 +0700

    bo check loi sort

---
+++ src/pages/checkouts.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Checkout() {
+  return (
+    <>
+      <Head>
+        <title>TopPay - Checkouts</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Checkouts</main>
+    </>
+  )
+}
